053156646cb79998c109c971ccc6c1ffc1bed0ad
Prateek Thakur <prateekthakur272@gmail.com>
2024-05-12 21:02:30 +0530

    adds neu box

---
+++ lib/src/widgets/neu_box.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:music_player_ui/src/extensions/build_context_extension.dart';
+
+class NeuBox extends StatelessWidget{
+  final Widget child;
+  const NeuBox({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      decoration: BoxDecoration(
+        color: context.colorScheme.background,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade500,
+            blurRadius: 15,
+            offset: const Offset(4, 4)
+          ),
+          const BoxShadow(
+              color: Colors.white,
+              blurRadius: 15,
+              offset: Offset(-4, -4)
+          ),
+        ]
+      ),
+      child: child,
+    );
+  }
+}
